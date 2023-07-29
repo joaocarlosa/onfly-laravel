34b0029265a65d2864d86764c1b04eff0fcfefb9
joaocarlosa <joaocarloslima99@gmail.com>
2023-07-29 20:42:41 -0300

    implement expense entity

---
+++ app/Http/Requests/StoreExpenseRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreExpenseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'description' => 'required|string|max:191',
+            'date' => 'required|date',
+            'user_id' => 'required|exists:users,id',
+            'value' => 'required|numeric|min:0',
+        ];
+    }
+}
---
+++ app/Models/Expense.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'updated_at',
+        'created_at',
+        'user_id',
+        'value',
+    ];
+}
